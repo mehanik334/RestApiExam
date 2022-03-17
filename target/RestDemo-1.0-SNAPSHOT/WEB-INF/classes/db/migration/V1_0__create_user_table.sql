45f334d6dc725e75a5bb145ba2af38a82903ca33
meha3 <meha334@gmail.com>
2022-03-17 17:37:25 +0200

    change procfile4

--- target/RestDemo-1.0-SNAPSHOT/WEB-INF/classes/db/migration/V1_0__create_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user
-(
-    id         int          NOT NULL AUTO_INCREMENT primary key,
-    first_name varchar(255) NOT NULL,
-    last_name  varchar(255) NOT NULL,
-    password   varchar(255) NOT NULL
-
-) ENGINE = InnoDB
-  DEFAULT CHARSET = UTF8;
